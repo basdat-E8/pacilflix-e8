7ace526b222cf627aa8c5e3ded0410431466eaff
raissaputi <raissa.puti@gmail.com>
2024-05-14 15:59:19 +0700

    connect to supabase

---
+++ utils/query.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extras import RealDictCursor
+import os
+import environ
+
+try:
+    connection = psycopg2.connect(user=os.getenv("DB_USERNAME"),
+                                password=os.getenv("DB_PASSWORD"),
+                                host=os.getenv("DB_HOST"),
+                                port=os.getenv("DB_PORT"),
+                                database=os.getenv("DB_NAME"))
+
+    # Create a cursor to perform database operations
+    connection.autocommit = True
+    cursor = connection.cursor()
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL: ", error)
+
+
+def map_cursor(cursor):
+    """Return all rows from a cursor as a namedtuple"""
+    description = cursor.description
+    named_tuple_result = namedtuple("Result", [col[0] for col in description])
+    return [dict(row) for row in cursor.fetchall()]
+
+
+def query(query_str: str, parameter: tuple = tuple()):
+    result = []
+    with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute("SET search_path TO pacilflix")
+        try:
+            cursor.execute(query_str, parameter)
+            # Handling SELECT queries
+            if query_str.strip().upper().startswith("SELECT"):
+                result = map_cursor(cursor)
+            else:
+                # Returns modified row count
+                result = cursor.rowcount
+                connection.commit()
+        except Exception as e:
+            result = e
+
+    return result
